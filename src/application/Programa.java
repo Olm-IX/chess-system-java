960bb7ce294c36fd38964827c0493b56a46b4eea
Naila Silva <nailaoli@gmail.com>
2019-05-27 13:21:55 -0300

    First class: Position

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardGame.Position;
+
 public class Programa {
 
 	public static void main(String[] args) {
 
-		System.out.println("sfsdfasf");
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 
 	}
 
